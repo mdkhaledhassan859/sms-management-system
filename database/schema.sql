9f48dac1ea8ae0e61b6a84d6a3bfbbedfeb439a0
mdkhaledhassan859 <mdkhaledhassan859@gmail.com>
2026-06-24 16:32:34 +0600

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -23,3 +23,23 @@ CREATE TABLE sms_logs (
     FOREIGN KEY (queue_id) REFERENCES sms_queue(id)
         ON DELETE SET NULL
 );
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sms_usage (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NULL,
+    total_sent INT DEFAULT 0,
+    total_failed INT DEFAULT 0,
+    date DATE NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE SET NULL
+);

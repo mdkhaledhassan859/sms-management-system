b70f12a0cabdc19e858655a60fd14b11a48548a0
mdkhaledhassan859 <mdkhaledhassan859@gmail.com>
2026-06-24 16:25:26 +0600

    Create SmsService.php

---
+++ core/SmsService.php
@@ -0,0 +1,27 @@
+<?php
+
+class SmsService {
+
+    public function send($to, $message) {
+        $config = include __DIR__ . '/../config/config.php';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $config['api_url'],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => http_build_query([
+                'user' => $config['sms_user'],
+                'password' => $config['sms_password'],
+                'to' => $to,
+                'text' => $message
+            ])
+        ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+}
